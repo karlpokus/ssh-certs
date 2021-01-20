547a451f0ca12b18b7421fa6a782658d5b9685d7
Karl Pokus <karl.pokus@gmail.com>
2021-01-20 13:53:38 +0100

    add role arg

--- sign.sh
+++ sign.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-vault write -field=signed_key ssh-client-signer/sign/dev \
+ROLE=$1 # dev nottl
+
+if test -z $ROLE; then
+  echo "error: missing ROLE arg at pos 1"
+  exit 1
+fi
+
+vault write -field=signed_key ssh-client-signer/sign/$ROLE \
 public_key=@$HOME/.ssh/test/id_rsa.pub > ~/.ssh/test/id_rsa-cert.pub
 
 result=$?
-
 if test $result -eq 0; then
   echo signed
   exit $result
