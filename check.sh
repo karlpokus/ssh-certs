0a17774c2630ce6f335db8f53cacd90c6375b25a
Karl Pokus <karl.pokus@gmail.com>
2021-01-22 22:14:48 +0100

    short note on check script usage

--- check.sh
+++ check.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# this thing prints the hostname on success and nothing on failure.
 ssh vagrant@127.0.0.1 -p 2222 -i ~/.ssh/test/id_rsa -i ~/.ssh/test/id_rsa-cert.pub -o StrictHostKeyChecking=no hostname 2> /dev/null
